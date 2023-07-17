384e11a9f76fc0ea07b56f1b6cf103860a335c1b
牧荻 <yaomodi.ymd@alibaba-inc.com>
2023-07-17 15:09:48 +0800

    hook-test

---
+++ print_http_body.rb
@@ -0,0 +1,9 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new(:Port => ARGV.first)
+server.mount_proc '/' do |req, res|
+  puts req.body
+end
+trap 'INT' do
+  server.shutdown
+end
+server.start
